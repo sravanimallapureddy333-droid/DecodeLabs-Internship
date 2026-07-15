3b52f7ac0a5ed31d52053c142b4317f6b7846474
sravanimallapureddy333-droid <sravanimallapureddy.333@gmail.com>
2026-07-15 18:09:05 +0530

    Add files via upload

---
+++ Project-1-Smart-Environmental-Monitor/Smart_Environmental_Monitor(1).ino
@@ -0,0 +1,12 @@
+void setup() {
+    Serial.begin(115200);
+      Serial.println("IoT Environment Monitoring System");
+      }
+
+      void loop() {
+        Serial.println("Temperature: 28 C");
+          Serial.println("Humidity: 60 %");
+            Serial.println("Status: Normal");
+              Serial.println("-------------------");
+                delay(2000);
+                }
